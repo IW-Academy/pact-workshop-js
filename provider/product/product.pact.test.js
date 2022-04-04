5164d37ec31db1aab95aa16cbc7d2af4626e879a
vincelee888 <vincent.s.lee@infinityworks.com>
2022-04-04 15:24:12 +0100

    makes step 7 interactive

--- provider/product/product.pact.test.js
+++ provider/product/product.pact.test.js
@@ -25,16 +25,13 @@ describe("Pact Verification", () => {
                     ]);
                 },
                 "products exist": () => {
-                    controller.repository.products = new Map([
-                        ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-                        ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
-                    ]);
+                    // todo: setup state for when products exist
                 },
                 "no products exist": () => {
-                    controller.repository.products = new Map();
+                    // todo: setup state for when no products exist
                 },
                 "product with ID 11 does not exist": () => {
-                    controller.repository.products = new Map();
+                    // todo: setup state for when no products exist
                 },
             }
         };
