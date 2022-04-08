902552f493d58f4e2a480b4c1bfae74d62b322be
vincelee888 <vincent.s.lee@infinityworks.com>
2022-04-08 17:23:25 +0100

    make step 3 interactive

--- consumer/src/api.pact.spec.js
+++ consumer/src/api.pact.spec.js
@@ -56,38 +56,10 @@ describe("API Pact test", () => {
 
     describe("getting one product", () => {
         test("ID 10 exists", async () => {
-
-            // set up Pact interactions
-            await provider.addInteraction({
-                state: 'product with ID 10 exists',
-                uponReceiving: 'get product with ID 10',
-                withRequest: {
-                    method: 'GET',
-                    path: '/products/10'
-                },
-                willRespondWith: {
-                    status: 200,
-                    headers: {
-                        'Content-Type': 'application/json; charset=utf-8'
-                    },
-                    body: like({
-                        id: "10",
-                        type: "CREDIT_CARD",
-                        name: "28 Degrees"
-                    }),
-                },
-            });
-
-            const api = new API(provider.mockService.baseUrl);
-
-            // make request to Pact mock server
-            const product = await api.getProduct("10");
-
-            expect(product).toStrictEqual({
-                id: "10",
-                type: "CREDIT_CARD",
-                name: "28 Degrees"
-            });
+            // GIVEN product with id 10 exists
+            // WHEN I request product with id 10
+            // THEN I receive a 200 response
+            // AND I receive a product object in the body
         });
     });
 });
