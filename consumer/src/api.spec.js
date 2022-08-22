a51fa4edb130a48845a96a08a9727f5cbb1dac8f
vincelee888 <vincent.s.lee@infinityworks.com>
2022-08-22 15:58:13 +0100

    adds first test in step2

--- consumer/src/api.spec.js
+++ consumer/src/api.spec.js
@@ -4,7 +4,27 @@ import nock from "nock";
 describe("API", () => {
 
     test("get all products", async () => {
-        // todo
+        const products = [
+            {
+                "id": "9",
+                "type": "CREDIT_CARD",
+                "name": "GEM Visa",
+                "version": "v2"
+            },
+            {
+                "id": "10",
+                "type": "CREDIT_CARD",
+                "name": "28 Degrees",
+                "version": "v1"
+            }
+        ];
+        nock(API.url)
+            .get('/products')
+            .reply(200,
+                products,
+                {'Access-Control-Allow-Origin': '*'});
+        const respProducts = await API.getAllProducts();
+        expect(respProducts).toEqual(products);
     });
 
     test("get product ID 50", async () => {
